d2d252dc9fccaab15ba7b2c5d7fd13cf3ad3a350
Jeff Squyres <jeff@squyres.com>
2025-02-28 12:08:13 -0500

    Testing
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- runme.py
+++ runme.py
@@ -2,7 +2,7 @@
 
 import os
 
-for var in ['GITHUB_TOKEN', 'MY_PRECIOUS']:
+for var in ['GITHUB_TOKEN', 'MY_PRECIOUS_ENV_SECRET', 'MY_PRECIOUS_REPO_SECRET']:
     value = os.environ.get(var, None)
     if value is None:
         print(f"Did not find env var {var}")
